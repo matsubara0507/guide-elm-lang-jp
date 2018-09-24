49a9cc0b53e5492cd0dc85f7cc5d3073d406ff8e
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-09-24 16:50:18 +0900

    Test

---
+++ script/diff_evancz_guide.sh
@@ -0,0 +1,14 @@
+git checkout 11d30ee5f70dc71912e5858b7d144163054b79c4 # todo
+git remote add evancz https://github.com/evancz/guide.elm-lang.org.git
+git fetch evancz
+
+DIFF_FILES=`git diff HEAD...evancz/master --name-only | sed -e 's/^/\\\\\\\\n/' | xargs echo`
+TRAVIS_BUILD_URL="https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}"
+
+if test -z ${DIFF_FILES}; then
+  curl \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d "{\"content\": \"以下のファイルで原文の更新がありました。${DIFF_FILES} \n${TRAVIS_BUILD_URL}\"}" \
+    ${DISCORD_WEBHOOK_URL}
+fi
--- script/notify_discord_script.sh
+++
@@ -1,8 +0,0 @@
-DIFF_FILES=`git diff HEAD...evancz/master --name-only | sed -e 's/^/\\\\\\\\n/' | xargs echo`
-TRAVIS_BUILD_URL="https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}"
-
-curl \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{\"content\": \"以下のファイルで原文の更新がありました。${DIFF_FILES} \n${TRAVIS_BUILD_URL}\"}" \
-  ${DISCORD_WEBHOOK_URL}
