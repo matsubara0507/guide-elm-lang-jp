13350510a7c84f5752ae0f5d100693ac9007257d
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-09-24 16:54:18 +0900

    Test

--- script/diff_evancz_guide.sh
+++ script/diff_evancz_guide.sh
@@ -5,10 +5,13 @@ git fetch evancz
 DIFF_FILES=`git diff HEAD...evancz/master --name-only | sed -e 's/^/\\\\\\\\n/' | xargs echo`
 TRAVIS_BUILD_URL="https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}"
 
+echo ${DIFF_FILES}
+
 if test -z ${DIFF_FILES}; then
   curl \
     -H "Content-Type: application/json" \
     -X POST \
     -d "{\"content\": \"以下のファイルで原文の更新がありました。${DIFF_FILES} \n${TRAVIS_BUILD_URL}\"}" \
     ${DISCORD_WEBHOOK_URL}
+  exit 1
 fi
